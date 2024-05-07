87cc345948f9085435da3aab9bebdd66c36b9ae7
Andrei <andreisoriano456@gmail.com>
2024-05-07 11:39:19 +0800

    andrey

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$connect = mysqli_connect("localhost:3306", "root","", "social");
+if (mysqli_connect_error()){
+    echo "Failed to connect in mysql" .mysqli_connect_error();
+}else{
+    
+}
+?>
